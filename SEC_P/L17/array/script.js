b34cb13238428f8f629c17850f418ba6d3980642
govind <govindkumarjha2017@gmail.com>
2025-09-06 12:37:17 +0530

    8 SEPT

---
+++ SEC_P/L17/array/script.js
@@ -0,0 +1,23 @@
+
+// var arr = [3,4,18,34,"hello",["start",45,"end"]];
+
+// console.log(arr)
+
+var arr = [23,43,56,7];
+
+console.log(arr);
+
+// arr.push(12);
+// arr.push("hello");
+// arr.pop();
+
+// arr.shift()
+
+// arr.unshift("start")
+
+arr.splice(1,3,"hello","ahk","wdkh")
+
+console.log(arr)
+
+
+console.log(arr.indexOf("ahk"))
---
+++ SEC_P/L17/introToJs/script.js
@@ -0,0 +1,36 @@
+
+console.log("Hello")
+
+
+// var let const
+
+var x = 6576
+console.log(x)
+var x ;
+x=34
+console.log(x)
+
+
+let y = 12;
+// let y  can't do this
+y="hii";
+console.log(y)
+
+const z = 34;
+// const z; can't do this
+
+// z=45;
+console.log(z)
+
+
+{
+    var a=10;
+    let b = 20;
+    const c = 30;
+
+    console.log(a,b,c)
+}
+
+console.log(a);
+console.log(b);
+console.log(c)
