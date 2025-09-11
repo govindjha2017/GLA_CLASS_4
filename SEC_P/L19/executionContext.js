57350f0308d32900bbd170753982355c8b4d7d80
govind <govindkumarjha2017@gmail.com>
2025-09-11 12:00:57 +0530

    11 SEPT SEC P

---
+++ SEC_P/L19/executionContext.js
@@ -0,0 +1,11 @@
+var x = 45;
+var money = 20;
+
+function fun(){
+    var a=20;
+    console.log(a);
+    console.log(money);
+}
+fun()
+console.log(x);
+console.log(a);
---
+++ SEC_P/L19/temp.js
@@ -0,0 +1,16 @@
+
+var a = "hello ";
+var b = "kaise ho";
+var c = a+b;
+console.log(c);
+
+// console.log(3+4);
+
+// console.log(3+"4");
+// console.log(3+4+"4"+5);
+// console.log(3+4+"4"-5);
+
+// console.log(3+4+"4"*5);
+
+console.log(3+4/2+'1'+4*3);
+
