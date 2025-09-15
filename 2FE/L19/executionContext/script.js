d33676a280ded7f6021e8acee5eb1faaad0629a6
govind <govindkumarjha2017@gmail.com>
2025-09-15 08:59:10 +0530

    15 SEPT SEC 2FE

---
+++ 2FE/L19/executionContext/script.js
@@ -0,0 +1,15 @@
+console.log(x);
+var x = 10;
+var money = 50;
+function fun(){
+    console.log(a);
+    var a = 10;
+    console.log(x);
+    console.log(money);
+    var money = 50;
+    console.log(money);
+}
+
+fun();
+console.log(money);
+console.log(a);
