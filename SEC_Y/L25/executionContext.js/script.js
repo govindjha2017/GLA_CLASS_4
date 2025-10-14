fdeafe0e2c329b943a4ee7ed0f434762dd7d56f8
govind <govindkumarjha2017@gmail.com>
2025-10-14 14:59:29 +0530

    14 oct SEC Y & S

---
+++ SEC_Y/L25/executionContext.js/script.js
@@ -0,0 +1,11 @@
+var x = 20;
+
+function fun(){
+    console.log(a);
+    var a=20;
+    console.log(x);
+    console.log(a)
+}
+fun()
+console.log(a);
+console.log("END");
---
+++ SEC_Y/L25/hosting/script.js
@@ -0,0 +1,9 @@
+
+console.log(x);
+var x = 20;
+console.log(x);
+
+console.log(y);
+let y= 20;
+console.log(y);
+ 
